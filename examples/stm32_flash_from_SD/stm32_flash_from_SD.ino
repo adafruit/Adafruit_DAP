eedcf1b8fa641ce074d551f0d5c284dc110fe818
hathach <thach@tinyusb.org>
2019-10-28 17:33:40 +0700

    add flash from SD example

--- examples/stm32_flash_from_SD/stm32_flash_from_SD.ino
+++ examples/stm32_flash_from_SD/stm32_flash_from_SD.ino
@@ -1,8 +1,14 @@
 #include "Adafruit_DAP.h"
+#include <SPI.h>
+#include <SD.h>
+
+#define SD_CS 10
 
 #define SWDIO 11
 #define SWCLK 12
 #define SWRST 13
+
+#define FILENAME "FIRMWARE.BIN"
 
 // the more the better
 #define BUFSIZE   (16*1024)
@@ -22,30 +28,6 @@ void error(const char *text) {
   while (1);
 }
 
-// dumping stm32 memory for verification
-void print_memory(uint32_t addr, uint8_t* buffer, uint32_t bufsize)
-{
-  memset(buffer, 0xff, bufsize);
-  dap.dap_read_block(addr, buffer, bufsize);
-
-  for(uint32_t i=0; i < bufsize; i++)
-  {
-    if (i % 16 == 0) 
-    {
-      if ( i != 0 ) Serial.println();
-      // print offset
-      if ( i < 0x100 ) Serial.print("0");
-      if ( i < 0x10  ) Serial.print("0");
-      Serial.print(i, HEX);
-      Serial.print(": ");
-    }
-
-    if ( buffer[i] < 0x10 ) Serial.print("0");
-    Serial.print(buffer[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.println();  
-}
 
 void setup() {
   pinMode(13, OUTPUT);
@@ -55,20 +37,32 @@ void setup() {
   }
 
   dap.begin(SWCLK, SWDIO, SWRST, &error);
+  
+    // see if the card is present and can be initialized:
+  if (!SD.begin(SD_CS)) {
+    error("Card failed, or not present");
+  }
+  Serial.println("Card initialized");
 
-  Serial.print("Connecting...");
+  File dataFile = SD.open(FILENAME);
+
+  if(!dataFile){
+     error("Couldn't open file");
+  }
+  
+  Serial.print("Connecting...");  
   if (! dap.dap_disconnect())                      error(dap.error_message);
-
+  
   char debuggername[100];
   if (! dap.dap_get_debugger_info(debuggername))   error(dap.error_message);
   Serial.print(debuggername); Serial.print("\n\r");
-
+  
   if (! dap.dap_connect())                         error(dap.error_message);
-
+  
   if (! dap.dap_transfer_configure(0, 128, 128))   error(dap.error_message);
   if (! dap.dap_swd_configure(0))                  error(dap.error_message);
   if (! dap.dap_reset_link())                      error(dap.error_message);
-  if (! dap.dap_swj_clock(50))                     error(dap.error_message);
+  if (! dap.dap_swj_clock(50))               error(dap.error_message);
   dap.dap_target_prepare();
 
   uint32_t dsu_did;
@@ -81,61 +75,38 @@ void setup() {
   Serial.print("Flash size\t");
   Serial.print(dap.target_device.flash_size / 1024);
   Serial.println(" KBs");
-  
-  uint32_t start_ms, duaration;
+
 
   //------------- Preparing sectors -------------//
+  uint32_t start_ms;
   Serial.print("Preparing ... ");
-  
   start_ms = millis();
 
   // preparing flash sector with address = 0, size = Binary size
-  dap.programPrepare(FLASH_START_ADDR, sizeof(buf));
-  duaration = millis()-start_ms;
+  dap.programPrepare(FLASH_START_ADDR, dataFile.size());
   
   Serial.print(" done in ");
-  Serial.print(duaration);
+  Serial.print(millis()-start_ms);
   Serial.println(" ms");
-
-  // verify if all memory is 0xff
-  //print_memory(0, buf, sizeof(buf));
 
   //------------- Programming -------------//
   Serial.print("Programming ");
-  Serial.print(sizeof(buf)/1024);
-  Serial.print(" KBs ...");
-  
-  // prepare data
-  for(uint32_t i=0; i<sizeof(buf); i++) buf[i] = i;
+  Serial.print(dataFile.size());
+  Serial.print(" bytes ...");
 
-  start_ms = millis();
-  dap.programBlock(FLASH_START_ADDR, buf, sizeof(buf));
-  
-  duaration = millis()-start_ms;
-  Serial.print(" done in ");
-  Serial.print(duaration);
-  Serial.print(" ms, ");
+  uint32_t addr = FLASH_START_ADDR;
 
-  Serial.print("Speed ");
-  Serial.print((double) sizeof(buf)/(duaration*1.024) );
-  Serial.println(" KBs/s");
-
-  //------------- Veify contents -------------//
-  start_ms = millis();
-  Serial.print("Verifying ... ");
-  if ( dap.verifyFlash(FLASH_START_ADDR, buf, sizeof(buf)) ) {
-    Serial.print("Matched");
-  }else {
-    Serial.print("MisMatched");
+  while (dataFile.available()) {
+      memset(buf, BUFSIZE, 0xFF);  // empty it out
+      uint32_t count = dataFile.read(buf, BUFSIZE);
+      dap.programBlock(addr, buf, count);
+      addr += count;
   }
-  
-  duaration = millis()-start_ms;
-  Serial.print(" done in ");
-  Serial.print(duaration);
-  Serial.print(" ms, ");
+  dataFile.close();
 
-  // verify if data is written
-  //print_memory(0, buf, sizeof(buf));
+  Serial.print(" done in ");
+  Serial.print(millis()-start_ms);
+  Serial.println(" ms");
 
   dap.deselect();
   dap.dap_disconnect();
@@ -144,7 +115,7 @@ void setup() {
 void loop() {
   //blink led on the host to show we're done
   digitalWrite(13, HIGH);
-  delay(500);
+  delay(500); 
   digitalWrite(13, LOW);
-  delay(500);
+  delay(500);  
 }
