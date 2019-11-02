abadb9bf961e8e7a88a88e9eaf6a99a59287ec32
Lady Ada <limor@ladyada.net>
2019-11-02 19:58:23 -0400

    compiling example

--- examples/flash_from_SPI_flash/flash_from_SPI_flash.ino
+++ examples/flash_from_SPI_flash/flash_from_SPI_flash.ino
@@ -4,7 +4,15 @@
 
 #define FILENAME "fw.bin"
 
+#define SWDIO 10
+#define SWCLK 9
+#define SWRST 11
+
 #define BUFSIZE 256 //don't change
+uint8_t buf[BUFSIZE];
+
+//create a DAP for programming Atmel SAM devices
+Adafruit_DAP_SAM dap;
 
 // Configuration of the flash chip pins and flash fatfs object.
 // You don't normally need to change these if using a Feather/Metro
@@ -23,19 +31,21 @@ Adafruit_SPIFlash flash(&flashTransport);
 // file system object from SdFat
 FatFileSystem fatfs;
 
-//create a seesaw with m0 DAP support
-dap_m0p dap;
+// Function called when there's an SWD error
+void error(const char *text) {
+  Serial.println(text);
+  while (1);
+}
 
-//create the target options
-//TODO: set some actual options here
-options_t g_target_options;
 
 void setup() {
   pinMode(13, OUTPUT);
-  Serial.begin(9600);
-  while(!Serial);
+  Serial.begin(115200);
+  while(!Serial) {
+    delay(1);         // will pause the chip until it opens serial console
+  }
 
-  dap.begin(9,10,11);
+  dap.begin(SWCLK, SWDIO, SWRST, &error);
   
   // Initialize flash library and check its chip ID.
   if (!flash.begin()) {
@@ -54,40 +64,70 @@ void setup() {
   Serial.println("Mounted filesystem!");
 
   File dataFile = fatfs.open(FILENAME, FILE_READ);
-  uint8_t buf[256];
+  if(!dataFile){
+     error("Couldn't open file");
+  }
+  
+  Serial.print("Connecting...");  
+  if (! dap.dap_disconnect())                      error(dap.error_message);
+  
+  char debuggername[100];
+  if (! dap.dap_get_debugger_info(debuggername))   error(dap.error_message);
+  Serial.print(debuggername); Serial.print("\n\r");
+  
+  if (! dap.dap_connect())                         error(dap.error_message);
+  
+  if (! dap.dap_transfer_configure(0, 128, 128))   error(dap.error_message);
+  if (! dap.dap_swd_configure(0))                  error(dap.error_message);
+  if (! dap.dap_reset_link())                      error(dap.error_message);
+  if (! dap.dap_swj_clock(50))               error(dap.error_message);
+  dap.dap_target_prepare();
 
-  if(dataFile){
-    dap.dap_disconnect();
-    dap.dap_get_debugger_info();
-    dap.dap_connect();
-    dap.dap_transfer_configure(0, 128, 128);
-    dap.dap_swd_configure(0);
-    dap.dap_reset_link();
-    dap.dap_swj_clock(DAP_FREQ);
-    dap.dap_target_prepare();
-  
-    dap.select(&g_target_options);
-  
-    Serial.print("Erasing... ");
-    dap.erase();
-    Serial.println(" done.");
-  
-    Serial.print("Programming... ");
-    uint32_t addr = dap.program_start();
-
-    while (dataFile.available()) {
-      dataFile.read(buf, 256);
-      addr = dap.program(addr, buf);
+  uint32_t dsu_did;
+  if (! dap.select(&dsu_did)) {
+    Serial.print("Unknown device found 0x"); Serial.print(dsu_did, HEX);
+    error("Unknown device found");
+  }
+  for (device_t *device = dap.devices; device->dsu_did > 0; device++) {
+    if (device->dsu_did == dsu_did) {
+      Serial.print("Found Target: ");
+      Serial.print(device->name);
+      Serial.print("\tFlash size: ");
+      Serial.print(device->flash_size);
+      Serial.print("\tFlash pages: ");
+      Serial.println(device->n_pages);
+      //Serial.print("Page size: "); Serial.println(device->flash_size / device->n_pages);
     }
-    dataFile.close();
-    
-    Serial.println(" done.");
   }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening file");
-    return;
+
+  /* Example of how to read and set fuses
+  Serial.print("Fuses... ");
+  dap.fuseRead(); //MUST READ FUSES BEFORE SETTING OR WRITING ANY
+  dap._USER_ROW.WDT_Period = 0x0A;
+  dap.fuseWrite();
+  */
+        
+  Serial.println(" done.");
+
+  Serial.print("Erasing... ");
+  dap.erase();
+  Serial.println(" done.");
+  
+  Serial.print("Programming... ");
+  unsigned long t = millis();
+  uint32_t addr = dap.program_start();
+
+  while (dataFile.available()) {
+      memset(buf, BUFSIZE, 0xFF);  // empty it out
+      dataFile.read(buf, BUFSIZE);
+      dap.programBlock(addr, buf);
+      addr += BUFSIZE;
   }
+  dataFile.close();
+  Serial.println(millis() - t);
+  Serial.println("\nDone!");
+  dap.dap_set_clock(50);
+
   dap.deselect();
   dap.dap_disconnect();
 }
