9a9ae73b786c480ba9ce8059e211dc224054cc96
ksmith3036 <44052735+ksmith3036@users.noreply.github.com>
2020-12-27 15:38:06 +0100

    Restructured code to support Reset Extension on SAMD21/51, for locked (protected) MCUs. dap_target_prepare should only be called in select, since it must be executed after Reset Extension for locked MCUs. Created targetConnect() method, since all sketches used excatly the same code to connect debugger to target MCU. Init is therefore simplified in sketches into dap.targetConnect and dap.select(...), to make sure the steps happen in the correct order. Moved target_device declaration to Adafruit_DAP, since it should be used by all implementing classes. For SAMD21/51: Moved definition of buffer sizes used into the classes, since reads and writes should match the correct page size. This also makes it possible to have common methods for flashing, using virtual methods. For SAMD21/51: Created a common method to copy and verify flash from a memory buffer (flash from header), utilizing virtual methods. For SAMD21: Corrected buggy handling of User Row fuses. The old way zeroized BOD12 Voltage Regulator configuration. This must be preserved, as it is calibrated from factory! For SAMD21: Temporarily turn off region locks while flashing. Resetting Bootloader protection, if necessary. For SAMD51: Corrected size of user Row  NVM_LOCKS from 16 to 32 bit. For SAMD51: Temporarily turn off region locks and bootloader protection while flashing.

--- examples/flash_from_SPI_flash/flash_from_SPI_flash.ino
+++ examples/flash_from_SPI_flash/flash_from_SPI_flash.ino
@@ -8,7 +8,7 @@
 #define SWCLK 9
 #define SWRST 11
 
-#define BUFSIZE 256 //don't change
+const int BUFSIZE = Adafruit_DAP_SAM::PAGESIZE;
 uint8_t buf[BUFSIZE];
 
 //create a DAP for programming Atmel SAM devices
@@ -69,36 +69,19 @@ void setup() {
   }
   
   Serial.print("Connecting...");  
-  if (! dap.dap_disconnect())                      error(dap.error_message);
-  
-  char debuggername[100];
-  if (! dap.dap_get_debugger_info(debuggername))   error(dap.error_message);
-  Serial.print(debuggername); Serial.print("\n\r");
-  
-  if (! dap.dap_connect())                         error(dap.error_message);
-  
-  if (! dap.dap_transfer_configure(0, 128, 128))   error(dap.error_message);
-  if (! dap.dap_swd_configure(0))                  error(dap.error_message);
-  if (! dap.dap_reset_link())                      error(dap.error_message);
-  if (! dap.dap_swj_clock(50))                     error(dap.error_message);
-  dap.dap_target_prepare();
+  dap.targetConnect();
 
   uint32_t dsu_did;
   if (! dap.select(&dsu_did)) {
     Serial.print("Unknown device found 0x"); Serial.print(dsu_did, HEX);
     error("Unknown device found");
   }
-  for (device_t *device = dap.devices; device->dsu_did > 0; device++) {
-    if (device->dsu_did == dsu_did) {
-      Serial.print("Found Target: ");
-      Serial.print(device->name);
-      Serial.print("\tFlash size: ");
-      Serial.print(device->flash_size);
-      Serial.print("\tFlash pages: ");
-      Serial.println(device->n_pages);
-      //Serial.print("Page size: "); Serial.println(device->flash_size / device->n_pages);
-    }
-  }
+  Serial.print("Found Target: ");
+  Serial.print(dap.target_device.name);
+  Serial.print("\tFlash size: ");
+  Serial.print(dap.target_device.flash_size);
+  Serial.print("\tFlash pages: ");
+  Serial.println(dap.target_device.n_pages);
 
   /* Example of how to read and set fuses
   Serial.print("Fuses... ");
--- examples/flash_from_header/flash_from_header.ino
+++ examples/flash_from_header/flash_from_header.ino
@@ -5,9 +5,6 @@
 #define SWCLK 8
 #define SWRST 7
 
-#define BUFSIZE 256       //don't change!
-//uint8_t buf[BUFSIZE];
-
 //create a DAP for programming Atmel SAM devices
 Adafruit_DAP_SAM dap;
 
@@ -19,50 +16,35 @@ void error(const char *text) {
 
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   while(!Serial) {
     delay(1);         // will pause the chip until it opens serial console
   }
 
+  delay(2000);
+  Serial.println("Starting...");
+
   dap.begin(SWCLK, SWDIO, SWRST, &error);
-  
-  Serial.print("Connecting...");  
-  if (! dap.dap_disconnect())                      error(dap.error_message);
-  
-  char debuggername[100];
-  if (! dap.dap_get_debugger_info(debuggername))   error(dap.error_message);
-  Serial.print(debuggername); Serial.print("\n\r");
-  
-  if (! dap.dap_connect())                         error(dap.error_message);
-  
-  if (! dap.dap_transfer_configure(0, 128, 128))   error(dap.error_message);
-  if (! dap.dap_swd_configure(0))                  error(dap.error_message);
-  if (! dap.dap_reset_link())                      error(dap.error_message);
-  if (! dap.dap_swj_clock(50))               error(dap.error_message);
-  dap.dap_target_prepare();
+
+  dap.targetConnect();
 
   uint32_t dsu_did;
   if (! dap.select(&dsu_did)) {
     Serial.print("Unknown device found 0x"); Serial.print(dsu_did, HEX);
     error("Unknown device found");
   }
-  for (device_t *device = dap.devices; device->dsu_did > 0; device++) {
-    if (device->dsu_did == dsu_did) {
-      Serial.print("Found Target: ");
-      Serial.print(device->name);
-      Serial.print("\tFlash size: ");
-      Serial.print(device->flash_size);
-      Serial.print("\tFlash pages: ");
-      Serial.println(device->n_pages);
-      //Serial.print("Page size: "); Serial.println(device->flash_size / device->n_pages);
-    }
-  }
+  Serial.print("Found Target: ");
+  Serial.print(dap.target_device.name);
+  Serial.print("\tFlash size: ");
+  Serial.print(dap.target_device.flash_size);
+  Serial.print("\tFlash pages: ");
+  Serial.println(dap.target_device.n_pages);
 
   /* Example of how to read and set fuses
   Serial.print("Fuses... ");
   dap.fuseRead(); //MUST READ FUSES BEFORE SETTING OR WRITING ANY
-  dap._USER_ROW.WDT_Period = 0x0A;
+  dap._USER_ROW.bit.WDT_Period = 0x0A;
   dap.fuseWrite();
   */
         
@@ -71,17 +53,14 @@ void setup() {
   Serial.print("Erasing... ");
   dap.erase();
   Serial.println(" done.");
-  
+
   Serial.print("Programming... ");
   unsigned long t = millis();
-  uint32_t addr = dap.program_start();
 
-  while(addr < sizeof(binfile)){
-    dap.programBlock(addr, binfile + addr);
-    addr += BUFSIZE;
-  }
-  
+  dap.programFlash(binfile, sizeof(binfile), true);
+
   Serial.println(millis() - t);
+  
   Serial.println("\nDone!");
   dap.dap_set_clock(50);
 
@@ -89,10 +68,11 @@ void setup() {
   dap.dap_disconnect();
 }
 
+
 void loop() {
   //blink led on the host to show we're done
-  digitalWrite(13, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(500); 
-  digitalWrite(13, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(500);  
 }
